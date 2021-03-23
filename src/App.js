099c90b740971f244e3ad2d3ba62bd0990330796
iroha0118 <ilovegumi0118@gmail.com>
2021-03-23 19:05:29 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 //chips: JSXとしてコンパイルする場合、import Reactは必要
 import React from 'react';
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">
-//           bar
-//         </label>
-//         <input type="text" onChange={() => {console.log('I am clicked!!')}}/>
-//       </React.Fragment>
-//     )
-//   }
-// }
 
+// chips: Userコンポーネントに対し、"name"という名前のprops(属性)を与えている
 const App = () => {
+
+  // chips2: これは多分標準のJavaScriptのコード
+  // chips3: NaNaeさんは年齢のpropsが存在しないため、
+  //         defaultPropsで定義した値が設定される。
+  const profiles = [
+    {
+      name: "Taro", age: 10
+    },
+    {
+      name: "Hanako", age: 5
+    },
+    {
+      name: "NaNae"
+    }
+  ]
+
+  // chips3: {}内にJavaScriptコーディングが出来る??
+  // chips4: 下記、key属性を指定しなくても問題ないが、ワーニングが出る。
+  //         これはユニークでないDOMツリーが出来てしまうため。
+  //         それを防ぐために、key属性でそれぞれ別のものだよ、っていうのをブラウザに教えることが出来る
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((el, index) => {
+          return <User name={el.name} age={el.age} key={index}/>
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+// chips: 下記の形を関数コンポーネントという。
+const User = (props) => {
+  return <div>Hi! I am {props.name}, and {props.age} years old! </div>
 }
 
+// chips5: コンポーネントのpropが不定である場合に、デフォルト値で以下を設定する..という意味
+User.defaultProps = {
+  age: 1
+}
 
 export default App;
